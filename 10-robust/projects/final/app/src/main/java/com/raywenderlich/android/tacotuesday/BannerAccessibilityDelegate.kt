446fdae914e332544e3b137aa52e3334a976ea79
Victoria Gonda <v.m.gonda@gmail.com>
2020-11-09 19:51:47 -0600

    Add starter project

---
+++ 10-robust/projects/final/app/src/main/java/com/raywenderlich/android/tacotuesday/BannerAccessibilityDelegate.kt
@@ -0,0 +1,17 @@
+package com.raywenderlich.android.tacotuesday
+
+import android.view.View
+import androidx.core.view.AccessibilityDelegateCompat
+import androidx.core.view.accessibility.AccessibilityNodeInfoCompat
+
+class BannerAccessibilityDelegate :
+    AccessibilityDelegateCompat() {
+
+  override fun onInitializeAccessibilityNodeInfo(
+      host: View?,
+      info: AccessibilityNodeInfoCompat?
+  ) {
+    super.onInitializeAccessibilityNodeInfo(host, info)
+    info?.roleDescription = "banner"
+  }
+}
